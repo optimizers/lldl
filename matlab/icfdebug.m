4f3d028eb4ac219a9c2ae903a41e2ab08a4cb428
Dominique Orban <dominique.orban@gmail.com>
2013-05-31 01:33:08 -0400

    Slightly more informative debug script.

--- matlab/icfdebug.m
+++ matlab/icfdebug.m
@@ -1,4 +1,20 @@
 beep off;
-load(['../tprobs/1138bus.mat']);
-Adiag = full(diag(A)); lA = tril(A,-1); p=5;
-[L, Ldiag] = icfmex(lA, Adiag, 5);
+format short e
+
+%load(['../tprobs/1138bus.mat']);
+n = 6; m = 4; E = rand(m,n);
+A = [(n+m+1)*eye(n) E' ; E -(n+m+1)*eye(m)];
+%A = 2*speye(n+m);
+%A = [1 0 1 ; 0 1 1 ; 1 1 -1 ];
+Adiag = full(diag(A)); lA = sparse(tril(A,-1)); p=0;  % Compute exact factor.
+
+% [L, Ldiag] = icfmex(lA, Adiag, p);
+% L = L + speye(size(L,1));       % L was the strict lower triangle only.
+
+% A - full(L) * diag(Ldiag) * full(L')
+
+for p = 0 : 4
+  [L, Ldiag] = icfmex(lA, Adiag, p);
+  L = L + speye(size(L,1));       % L was the strict lower triangle only.
+  full(L)
+end
