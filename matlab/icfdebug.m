7c92f906585d8e9b59da41376e11121244d26640
Dominique Orban <dominique.orban@gmail.com>
2013-06-03 15:29:52 -0400

    Rename Ldiag to D in demo script.

--- matlab/icfdebug.m
+++ matlab/icfdebug.m
@@ -1,5 +1,6 @@
 beep off;
 format short e
+randseed;
 
 %load(['../tprobs/1138bus.mat']);
 n = 6; m = 4; E = rand(m,n);
@@ -8,13 +9,13 @@ A = [(n+m+1)*eye(n) E' ; E -(n+m+1)*eye(m)];
 %A = [1 0 1 ; 0 1 1 ; 1 1 -1 ];
 Adiag = full(diag(A)); lA = sparse(tril(A,-1)); p=0;  % Compute exact factor.
 
-% [L, Ldiag] = icfmex(lA, Adiag, p);
+% [L, D] = icfmex(lA, Adiag, p);
 % L = L + speye(size(L,1));       % L was the strict lower triangle only.
 
-% A - full(L) * diag(Ldiag) * full(L')
+% A - full(L) * diag(D) * full(L')
 
 for p = 0 : 4
-  [L, Ldiag] = icfmex(lA, Adiag, p);
+  [L, D] = icfmex(lA, Adiag, p);
   L = L + speye(size(L,1));       % L was the strict lower triangle only.
   full(L)
 end
