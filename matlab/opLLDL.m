4470db23c52034cf513fd0edc5638f82c38638c0
Dominique Orban <dominique.orban@gmail.com>
2016-01-16 07:44:33 -0500

    matlab: indef and fill_factor

--- matlab/opLLDL.m
+++ matlab/opLLDL.m
@@ -1,7 +1,7 @@
 classdef opLLDL < opSpot
 %OPLLDL  Operator representing the preconditioner
 %
-%          L |D| L'
+%          L D L'   or   L |D| L'
 %
 %        where L and D are incomplete LDL factors of the SQD matrix K.
 %
@@ -11,6 +11,20 @@ classdef opLLDL < opSpot
 %   opLLDL(K,p,shift) is equivalent to opLLDL(K + shift * E, p), where
 %   E is diagonal with +1s and -1s on the diagonal, with the same
 %   definiteness pattern as K. The default value of shift is zero.
+%
+%   Options may be set by passing a structure as last argument:
+%   opLLDL(K,p,shift,opts) where
+%     opts.indef = whether the operator is allowed to be indefinite (true)
+%                  or not (false). If set to `true`, the diagonal D
+%                  resulting from the factorization is used as is when
+%                  applying the preconditioner. If set to `false`, then
+%                  |D| is used.
+%     opts.fill_factor = whether the argument `p` should be interpreted
+%                        as an additive (nnz + p*n) or multiplicative
+%                        (p * nnz) memory specification. In the latter
+%                        case (`opts.fill_factor = true`), an equivalent
+%                        additive value of p is computed to perform the
+%                        factorization.
 %
 %   Note that K is an explicit matrix. The public properties are:
 %
@@ -45,13 +59,15 @@ classdef opLLDL < opSpot
    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
    properties( SetAccess = private )
       L             % "Inverse" lower triangular incomplete factor
-      D             % "Inverse" absolute value of diagonal incomplete factor
+      D             % Currently used variant of the diagonal (signed or unsigned)
+      Dabs          % "Inverse" absolute value of diagonal incomplete factor
       Dinv          % "Inverse" diagonal incomplete factor (for normest)
       nnz           % Number of nonzeros in L
       p             % Limited-memory factor
       shift         % The shift necessary to complete the factorization
       growth        % Growth factor max(max |L|, max |D|) / max |K|
-      minpivot      % Smallest pivot in absolute value.
+      minpivot      % Smallest pivot in absolute value
+      indef         % Whether D should be replaced with |D|.
    end
 
    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -63,12 +79,26 @@ classdef opLLDL < opSpot
       % opLDL. Constructor
       %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-      function op = opLLDL(K, p, shift)
-         if nargin < 2 | nargin > 3
+      function op = opLLDL(K, p, shift, opts)
+         if nargin < 2 || nargin > 4
             error('Invalid number of arguments.');
          end
          if ~exist('shift', 'var') || isempty(shift)
             shift = 0;
+         end
+         indef = false;
+         fill_factor = false;
+         if exist('opts', 'var')
+            if isfield(opts, 'indef')
+               if islogical(opts.indef)
+                  indef = opts.indef;
+               end
+            end
+            if isfield(opts, 'fill_factor')
+               if islogical(opts.fill_factor)
+                  fill_factor = opts.fill_factor;
+               end
+            end
          end
 
          % Get size of input matrix
@@ -80,9 +110,22 @@ classdef opLLDL < opSpot
 
          % Construct operator
          op = op@opSpot('LLDL', n, n);
-         op.cflag         = false;
-         op.p             = max(p,0);
-         [L, D, op.shift] = lldl(sparse(tril(K,-1)), full(diag(K)), op.p, shift);
+         op.cflag   = false;
+         op.indef   = indef;
+
+         % If opts.fill_factor, p specifies the fill factor instead of
+         % the absolute additional memory.
+         % The total number of nonzers allowed in the factors is as in iLDL:
+         % fill/n * nnz(K). This gives
+         % fill/n * nnz(K) = nnz(K) + pn, i.e., p = (fill/n - 1) * nnz(K) / n
+         T = tril(K, -1);
+         if fill_factor
+           nnzK = nnz(T);
+           op.p = max(round((p * (n + 2 * nnzK) - nnzK) / n), 0);
+         else
+           op.p = max(p, 0);
+         end
+         [L, D, op.shift] = lldl(sparse(T), full(diag(K)), op.p, shift);
          absD             = abs(D);
          op.nnz           = nnz(L);
 
@@ -93,8 +136,13 @@ classdef opLLDL < opSpot
 
          op.minpivot      = min(absD);
          op.L             = inv(opMatrix(L));
-         op.D             = opDiag(1./absD);
-         op.Dinv          = opDiag(1./D);           % For op.normest().
+         op.Dabs          = opDiag(1./absD);
+         op.Dinv          = opDiag(1./D);
+         if indef
+           op.D           = op.Dinv;
+         else
+           op.D           = op.Dabs;
+         end
          op.sweepflag     = true;
       end % function opLLDL
 
