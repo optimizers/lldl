d15285219d2e7992125ddd31069b13b377925052
Dominique Orban <dominique.orban@gmail.com>
2021-03-18 14:41:47 -0400

    modernize install and Julia interface

--- julia/lldl.jl
+++ julia/lldl.jl
@@ -1,58 +1,56 @@
 # Julia interface to LLDL.
+using LinearOperators
 
 # liblldl.dylib should be in you LD_LIBRARY_PATH.
-lldl_lib = "liblldl.dylib";
+const liblldl = "liblldl.dylib"
 macro lldl_call(args...)
+  args = map(esc, args)
   quote
-    ccall((:dicfs_, $lldl_lib), Void, $(args...))
+    ccall((:dicfs_, liblldl), Nothing, $(args...))
   end
 end
 
-function lldl_base(K :: SparseMatrixCSC, p :: Int; droptol :: Float64=0.0, minpiv :: Float64=0.0)
-  n = size(K, 1);
-  adiag = diag(K);
-  A = sparse(tril(K, -1));
-  nnzA = nnz(A);
-  d = zeros(Float64, n);
-  l = zeros(Float64, nnzA + n * p);
-  lcolptr = zeros(Int32, n + 1);
-  lrowind = zeros(Int32, nnzA + n * p);
-  alpha = zeros(Float64, 1);  # May be modified by Fortran.
+function lldl_base(K::SparseMatrixCSC, p::Int; droptol::Float64=0.0, minpiv::Float64=0.0)
+  n = size(K, 1)
+  adiag = convert(Vector{Float64}, diag(K))
+  A = sparse(tril(K, -1))
+  nnzA = nnz(A)
+  d = zeros(n)
+  l = zeros(nnzA + n * p)
+  lcolptr = zeros(Int32, n + 1)
+  lrowind = zeros(Int32, nnzA + n * p)
+  alpha = zeros(1)  # May be modified by Fortran.
 
   # Temporary work arrays.
-  iwa = zeros(Int32, 3 * n, 1);
-  wa1 = zeros(Float64, n, 1);
-  wa2 = zeros(Float64, n, 1);
+  iwa = zeros(Int32, 3 * n)
+  wa1 = zeros(n)
+  wa2 = zeros(n)
 
-  @printf("droptol=%8.1e, minpiv=%8.1e\n", droptol, minpiv)
-
-  @lldl_call((Ptr{Int32},   Ptr{Int32},
-              Ptr{Float64}, Ptr{Float64}, Ptr{Int32},   Ptr{Int32},
-              Ptr{Float64}, Ptr{Float64}, Ptr{Int32},   Ptr{Int32},
-              Ptr{Int32},   Ptr{Float64}, Ptr{Float64}, Ptr{Float64},
-              Ptr{Int32},   Ptr{Float64}, Ptr{Float64}),
-              &n, &nnzA,
-              A.nzval, adiag, convert(Array{Int32, 1}, A.colptr), convert(Array{Int32, 1}, A.rowval),
+  @lldl_call((Ref{Int32},   Ref{Int32},
+              Ref{Float64}, Ref{Float64}, Ref{Int32},   Ref{Int32},
+              Ref{Float64}, Ref{Float64}, Ref{Int32},   Ref{Int32},
+              Ref{Int32},   Ref{Float64}, Ref{Float64}, Ref{Float64},
+              Ref{Int32},   Ref{Float64}, Ref{Float64}),
+              n, nnzA,
+              A.nzval, adiag, convert(Vector{Int32}, A.colptr), convert(Vector{Int32}, A.rowval),
               l, d, lcolptr, lrowind,
-              &p, alpha, &droptol, &minpiv,
-              iwa, wa1, wa2);
+              p, alpha, droptol, minpiv,
+              iwa, wa1, wa2)
 
-  return (l, lcolptr, lrowind, d, alpha[1]);
+  return (l, lcolptr, lrowind, d, alpha[1])
 end
 
-function lldl(K :: SparseMatrixCSC, p :: Int; droptol :: Float64=0.0, minpiv :: Float64=0.0)
-  n = size(K, 1);
-  (l, lcolptr, lrowind, d, alpha) = lldl_base(K, p, droptol=droptol, minpiv=minpiv);
-  L = SparseMatrixCSC(n, n, lcolptr, lrowind, l);
-  return (L, d, alpha);
+function lldl(K::SparseMatrixCSC, p::Int; droptol::Float64=0.0, minpiv::Float64=0.0)
+  n = size(K, 1)
+  (l, lcolptr, lrowind, d, alpha) = lldl_base(K, p, droptol=droptol, minpiv=minpiv)
+  L = SparseMatrixCSC(n, n, lcolptr, lrowind, l)
+  return (L, d, alpha)
 end
 
-using LinearOperators
-
-function lldl_op(K :: SparseMatrixCSC, p :: Int; droptol :: Float64=0.0, minpiv :: Float64=0.0)
-  n = size(K, 1);
-  (Lmat, d, alpha) = lldl(K, p, droptol=droptol, minpiv=minpiv);
-  D = opDiagonal(1./abs(d));
-  L = opInverse(Lmat + speye(n));
-  return (L' * D * L, alpha);
+function lldl_op(K::SparseMatrixCSC, p::Int; droptol::Float64=0.0, minpiv::Float64=0.0)
+  n = size(K, 1)
+  (Lmat, d, alpha) = lldl(K, p, droptol=droptol, minpiv=minpiv)
+  D = opDiagonal(1 ./ abs.(d))
+  L = opInverse(Lmat + spdiagm(0 => ones(n)))
+  return (L' * D * L, alpha)
 end
--- julia/lldl_test.jl
+++ julia/lldl_test.jl
@@ -1,14 +1,18 @@
 include("lldl.jl")
+using Printf
 
-n = 10;
-m = 6;
-E = sprand(m, n, .2);
-K = [(n+m+1)*speye(n) E' ; E  -(n+m+1)*speye(m)];
+n = 10
+m = 6
+E = sprand(m, n, .2)
+In = (n + m + 1) * spdiagm(0 => ones(n))
+Im = (n + m + 1) * spdiagm(0 => ones(m))
+K = [In E' ; E  -Im]
 
-@printf("p  ‖LDL'-K‖  κ(P*K)\n");
+@printf("p  ‖LDL'-K‖  κ(P*K)\n")
 for p = 1 : 5
-  (L, d, alpha) = lldl(K, p); L = L + speye(n+m);
-  (P, alpha) = lldl_op(K, p);
-  @printf("%1d  %8.2e  %8.2e\n", p, norm(L*diagm(d)*L' - K), cond(full(P * K)));
+  (L, d, alpha) = lldl(K, p)
+  L = L + I
+  (P, alpha) = lldl_op(K, p)
+  @printf("%1d  %8.2e  %8.2e\n", p, norm(L*diagm(d)*L' - K), cond(Matrix(P * K)))
 end
 
